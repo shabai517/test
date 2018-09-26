4376db463593efb35a11ed0cc1470dd0f8964dbf
shabai517 <shabai517@163.com>
2018-09-26 12:53:08 +0100

    test

--- config/index.js
+++ config/index.js
@@ -48,8 +48,8 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
-    //assetsPublicPath: '/test/',
+    //assetsPublicPath: '/',
+    assetsPublicPath: '/test/',
 
     /**
      * Source Maps
