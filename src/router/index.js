fa0501fb2aaf8cf1072e41949d606eda6c50118f
shabai517 <shabai517@163.com>
2018-09-26 11:36:06 +0100

    test

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ import Containerdetails from '@/components/Containerdetails'
 Vue.use(Router)
 
 export default new Router({
-  //mode: 'history',
+  mode: 'history',
   routes: [
     {
       path: '/',
