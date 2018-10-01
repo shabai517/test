326d013b0fac689b185a37ca458f8da2aba0fff3
shabai517 <shabai517@163.com>
2018-10-01 10:28:49 +0100

    123

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,19 @@ export default {
   name: 'App',
   components:{
     defaultNav,
-  }
+  },
+
+  mounted: function(){
+        var redirect = sessionStorage.redirect;
+        console.log('redirect',redirect);
+        delete sessionStorage.redirect;
+        if (redirect && redirect != location.href) {
+          let temp = location.pathname.split('/')[2];
+          console.log('temp',temp);
+          this.$router.replace({ path: temp });
+          //history.replaceState(null, null, redirect);
+        }
+  },
 }
 </script>
 
