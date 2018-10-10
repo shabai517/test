6d8c5556bdf55fea60958ffbdc8f592ab6bb12d4
shabai517 <shabai517@163.com>
2018-10-10 10:46:49 +0100

    build multpackage from react to vue

--- src/components/Multipackage.vue
+++ src/components/Multipackage.vue
@@ -1,13 +1,52 @@
 <template>
   <div class="multipackage-container">
-    <div class="banner">
+      <div class="banner">
           <div class="content-wrapper">
               <h1 class="title">Multi-package Containers</h1>
               <p class="description">Combine several conda packages into one Docker container.</p> 
           </div>
       </div>
-      <iframe class="lorikeet-iframe" src="http://biocontainers.pro/multi-package-containers/" scrolling="no"></iframe>
+      <div class="content-container">
+          <div class="packages-wrapper">
+              <Card class="card">
+                  <p slot="title">Packages</p>
+                  <div class="search-wrapper">
+                    <Input v-model="keywords" icon="ios-search" placeholder="Search" style="width:100%"></Input>
+                  </div>
+                  <Table stripe :columns="resultsTableCol" :data="resutls"></Table>
+                  <div class="page-wrapper">
+                      <Page :total="total" :current="current" :page-size="pageSize" size="small" show-elevator show-sizer @on-change="pageChange" @on-page-size-change="pageSizeChange"/>
+                  </div>
+              </Card>
+          </div>
+          <div v-if="selectedPackagesResutls.length>0" class="packages-wrapper selected-packages">
+              <Card class="card">
+                  <p slot="title">Selected Packages</p>
+                  <Alert v-if="containerRepeated" type="error">{{containerRepaetedName}} Container Repeated</Alert>
+                  <Table stripe :columns="selectedPackagesTableCol" :data="selectedPackagesResutls"></Table>
+                  <div class="button-wrapper">
+                      <Button type="error">Clear</Button>
+                  </div>
+                  <div class="details-wrapper">
+                      <p>To create a container, please add the following string to this file as a pull request:</p>
+                      <div class="textarea-wrapper"> 
+                        <Input v-model="createContainerValue" disabled type="textarea" placeholder="Enter something..." />
+                      </div>
+                      <p>Container name:</p>
+                      <div class="textarea-wrapper"> 
+                        <Input v-model="containerNameValue" disabled type="textarea" placeholder="Enter something..." />
+                      </div>
+                  </div>
+                  <div class="button-wrapper">
+                      <Button type="success">Download</Button>
+                  </div>
+              </Card>
+          </div>
+     
+      </div>
+      <!--<iframe class="lorikeet-iframe" src="http://biocontainers.pro/multi-package-containers/" scrolling="no"></iframe>-->
   </div>
+
 </template>
 
 <script>
@@ -16,117 +55,187 @@ export default {
   data () {
     return {
         keywords:'',
-        
+        total:5598,
+        current:1,
+        pageSize:20, 
+        containerRepeated:false,
+        containerRepaetedName:'',
+        createContainerValue: '2pg_cartesian=1.0.1,_license=1.1,_nb_ext_conf=0.3.0,abawaca=1.00,abricate=0.4,abstract-rendering=0.5.1,abundancebin=1.0.1',
+        containerNameValue:'quay.io/biocontainers/mulled-v2-87f6cdd7eada4e4f6f2fc092e7820d826d5aeeaa:5b8a8a59d2a5385e45cbe82909bf44e7544071f1',
         resultsTableCol:[
             {
-                title: 'Container',
-                key: 'container'
+                title: 'Name',
+                key: 'Name'
             },
             {
-                title: 'Description',
-                key: 'description'
+                title: 'Version',
+                key: 'Version'
             },
             {
-                title: 'Real Name',
-                key: 'realname'
+                title: 'Channel',
+                key: 'Channel'
             },
             {
-                title: 'Last Modified',
-                key: 'lastmodified'
-            },
-            {
-                title: 'Starred/Starts',
-                key: 'starredstarts'
-            },
-            {
-                title: 'Popularity',
-                key: 'popularity'
-            },
-            {
-                title: 'Registry Link',
-                key: 'registrylink'
-            },
+                title: '',
+                key: 'action',
+                align:'center',
+                width:100,
+                render: (h, params) => {
+                    return h('div', [
+                        /*
+                        h('Button', {
+                           
+                            on: {
+                                click: () => {
+                                    this.gotoBlast(params);
+                                }
+                            }
+                        }, 'Blast'),
+                        */
+                        h('Icon', {
+                            props: {
+                                type: 'checkmark-round',
+                            },
+                            style: {
+                                marginLeft: '5px'
+                            },
+                            on: {
+                                click: () => {
+                                    this.packagesRowClick(params.row);
+                                }
+                            }
+                        }),
+                    ]);
+                }
+            }
         ],
         resutls:[
             {
-                container: 'John Brown',
-                description: 18,
-                realname: 'New York No. 1 Lake Park',
-                lastmodified: '2016-10-03',
-                starredstarts:'test',
-                popularity:'test',
-                registrylink:'test'
+                Name: '2pg_cartesian',
+                Version: '1.0.1',
+                Channel: 'bioconda',
             },
             {
-                container: 'John Brown',
-                description: 18,
-                realname: 'New York No. 1 Lake Park',
-                lastmodified: '2016-10-03',
-                starredstarts:'test',
-                popularity:'test',
-                registrylink:'test'
+                Name: '_license',
+                Version: '1.1',
+                Channel: 'anaconda',
             },
             {
-                container: 'John Brown',
-                description: 18,
-                realname: 'New York No. 1 Lake Park',
-                lastmodified: '2016-10-03',
-                starredstarts:'test',
-                popularity:'test',
-                registrylink:'test'
+                Name: '_nb_ext_conf',
+                Version: '0.3.0',
+                Channel: 'anaconda',
             },
             {
-                container: 'John Brown',
-                description: 18,
-                realname: 'New York No. 1 Lake Park',
-                lastmodified: '2016-10-03',
-                starredstarts:'test',
-                popularity:'test',
-                registrylink:'test'
+                Name: 'abawaca',
+                Version: '1.00',
+                Channel: 'bioconda',
             },
             {
-                container: 'John Brown',
-                description: 18,
-                realname: 'New York No. 1 Lake Park',
-                lastmodified: '2016-10-03',
-                starredstarts:'test',
-                popularity:'test',
-                registrylink:'test'
+                Name: 'abricate',
+                Version: '0.4',
+                Channel: 'bioconda',
             },
             {
-                container: 'John Brown',
-                description: 18,
-                realname: 'New York No. 1 Lake Park',
-                lastmodified: '2016-10-03',
-                starredstarts:'test',
-                popularity:'test',
-                registrylink:'test'
+                Name: 'abstract-rendering',
+                Version: '0.5.1',
+                Channel: 'anaconda',
             },
             {
-                container: 'John Brown',
-                description: 18,
-                realname: 'New York No. 1 Lake Park',
-                lastmodified: '2016-10-03',
-                starredstarts:'test',
-                popularity:'test',
-                registrylink:'test'
+                Name: 'abundancebin',
+                Version: '1.0.1',
+                Channel: 'bioconda',
             },
             {
-                container: 'John Brown',
-                description: 18,
-                realname: 'New York No. 1 Lake Park',
-                lastmodified: '2016-10-03',
-                starredstarts:'test',
-                popularity:'test',
-                registrylink:'test'
+                Name: 'abyss',
+                Version: '2.0.1',
+                Channel: 'bioconda',
             },
-            
-        ]
+            {
+                Name: 'abyss-k128',
+                Version: '2.0.1',
+                Channel: 'bioconda',
+            },
+            {
+                Name: 'addict',
+                Version: '2.1.0',
+                Channel: 'conda-forge',
+            },
+        ],
+        selectedPackagesTableCol:[
+            {
+                title: 'Name',
+                key: 'Name'
+            },
+            {
+                title: 'Version',
+                key: 'Version'
+            },
+            {
+                title: '',
+                key: 'action',
+                align:'center',
+                width:100,
+                render: (h, params) => {
+                    return h('div', [
+                        /*
+                        h('Button', {
+                           
+                            on: {
+                                click: () => {
+                                    this.gotoBlast(params);
+                                }
+                            }
+                        }, 'Blast'),
+                        */
+                        h('Icon', {
+                            props: {
+                                type: 'checkmark-round',
+                            },
+                            style: {
+                                marginLeft: '5px'
+                            },
+                            on: {
+                                click: () => {
+                                    this.selectedPackagesRowClick(params.row);
+                                }
+                            }
+                        }),
+                    ]);
+                }
+            }
+        ],
+        selectedPackagesResutls:[],
     }
   },
   methods:{
-    rowClick(){
-        //this.$router.push({name:'ContainerDetails',params:{id:row.ID}});
+    packagesRowClick(rowItem){
+        var found = false;
+        for(let i in this.selectedPackagesResutls){
+          if(this.selectedPackagesResutls[i]&&this.selectedPackagesResutls[i].Name.match(rowItem.Name)){
+              found=true;
+              this.containerRepaetedName=rowItem.Name;
+              break;
+          }
+        }
+        if(!found){
+            var item={
+                Name:rowItem.Name,
+                Version:rowItem.Version
+            }
+            this.selectedPackagesResutls.push(item);
+        }
+        else{
+          this.containerRepeated=true;
+          setTimeout(()=>{
+            this.containerRepeated=false;
+            this.containerRepaetedName='';
+          },1500)
+        }
+        
+        
+    },
+    selectedPackagesRowClick(rowItem){
+      this.selectedPackagesResutls.splice(rowItem._index, 1);
     },
   	test(){
   		this.$http
@@ -137,10 +246,16 @@ export default {
             },function(err){
 
             });
-  	}
+  	},
+    pageChange(){
+
+    },
+    pageSizeChange(){
+
+    }
   },
   mounted(){
-    console.log('receive id',this.$route.params.id);
+    //console.log('receive id',this.$route.params.id);
   	//this.test();
   }
 }
@@ -148,6 +263,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
     .multipackage-container{
          width: 100%;
          height: 100%;
@@ -163,6 +279,11 @@ export default {
       color: #ffffff;
       padding: 3rem 0;
     }
+    .search-wrapper{
+      width: 100%;
+      text-align: center;
+      margin: 0 auto 10px auto;
+    }
     .content-wrapper{
       width: 80%;
       padding-right: 15px;
@@ -173,10 +294,37 @@ export default {
       flex-wrap: wrap;
       -ms-flex-wrap: wrap;
     }
+    .content-container{
+      width: 90%;
+      text-align: center;
+      margin: 30px auto;
+      display: flex;
+    }
+    .selected-packages{
+      margin-left: 10px !important;
+    }
+    .packages-wrapper{
+      width: 50%;
+      margin: 0 auto;
+      display: inline-block;
+    }
     .title{
       font-size: 4.5rem;
       font-weight: 300;
       line-height: 1.2;
+    }
+    .page-wrapper{
+      margin-top: 10px;
+    }
+    .card p{
+      text-align: left;
+    }
+    .button-wrapper{
+      margin:20px auto;
+    }
+    .textarea-wrapper{
+      margin: 5px 0;
+      white-space: nowrap;
     }
 </style>
 
