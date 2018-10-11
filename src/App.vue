af5811e9dcd47b554c878b95d088088c4772f7ef
shabai517 <shabai517@163.com>
2018-10-11 11:34:44 +0100

    add docker icon

--- src/App.vue
+++ src/App.vue
@@ -34,4 +34,10 @@ html,body {
   -moz-osx-font-smoothing: grayscale;
   height: 100%;
 }
+.icon {
+  width: 1em; height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
 </style>
